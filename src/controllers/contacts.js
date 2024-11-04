b958b156bcb80ac0cd24f8c64ceabeddff20fb35
melodiam16 <121240405+melodiam16@users.noreply.github.com>
2024-11-04 11:36:50 +0100

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,7 +48,7 @@ export const patchContactController = async (req, res) => {
   res.json({
     status: 200,
     message: 'Successfully patched a contact!',
-    data: result.data,
+    data: result,
   });
 };
 
