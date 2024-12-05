7c19172909f7d4b68d2cce0795a00a2d7bee367c
melodiam16 <121240405+melodiam16@users.noreply.github.com>
2024-12-05 19:50:02 +0100

    update

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -34,7 +34,7 @@ authRouter.post(
 authRouter.post('/logout', ctrlWrapper(authControllers.logoutController));
 
 authRouter.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(authControllers.requestResetEmailController),
 );
